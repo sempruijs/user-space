37261a7aa6f8b24ea2288348970f607fd041ffe0
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-23 16:47:00 +0200

    no styles sign in / up form

--- site/ts/login.ts
+++ site/ts/login.ts
@@ -1,8 +1,39 @@
+// toggles between signin / signup form
+function toggleFormType(): void {
+    const form = document.getElementById("credential-form");
+    const optionalContent = form.children.namedItem("optional-form-content");
+
+    if (form.classList.contains("signin")) {
+        optionalContent.classList.remove("hidden");
+        form.classList.remove("signin");
+    }
+    else {
+        optionalContent.classList.add("hidden");
+        form.classList.add("signin");
+    }
+}
+
 function load(): void {
+
+    const credentialForm: HTMLElement = document.getElementById("credential-form");
+    credentialForm.addEventListener("submit", event => {
+        // intercept form action
+        event.preventDefault();
+    
+        // extract form data
+        const data = new FormData(credentialForm as HTMLFormElement);
+
+        const isSignInForm = credentialForm.classList.contains("signin");
+
+        // console.log(data, isSignInForm);
+
+       // TODO send data 
+    });
 
     // check if login credentials are saved
     // and yes I am storing the password in plaintext in a cookie
     // deal with it
+    const username = getCookie("username");
 
 }
 
