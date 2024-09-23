8001e6c66b26cad1ba11533c46653515878ce750
tobia <tobiaslbeumer@gmail.com>
2024-09-23 16:29:43 +0200

    bug fixes

--- site/ts/99_index.ts
+++ site/ts/99_index.ts
@@ -1,8 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.querySelector('.sidebar');
     const arrow = document.getElementById('sidebar-toggle');
+    const profilesBtn = document.getElementById('profiles-btn');
+    const scrumBtn = document.getElementById('scrum-btn');
+    const hboIBtn = document.getElementById('hbo-i-btn');
+    const homeBtn = document.getElementById('home-btn');
 
     arrow.addEventListener('click', function() {
         sidebar.classList.toggle('sidebar-expanded');
     });
+
+    if(profilesBtn){
+        profilesBtn.addEventListener('click', function() {
+            window.location.href = 'profiles.html';
+        })
+    } else if (scrumBtn){
+        scrumBtn.addEventListener('click', function() {
+            window.location.href = 'scrum.html';
+        })
+    } else if (hboIBtn){
+        hboIBtn.addEventListener('click', function() {
+            window.location.href = 'hboI.html';
+        })
+    } else if (homeBtn){
+        homeBtn.addEventListener('click', function() {
+            window.location.href = 'index.html';
+        })
+    }
 });
