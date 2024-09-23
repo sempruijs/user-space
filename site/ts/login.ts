09a7f4a3612b00ac5e97dacf718ebc9b542d113b
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-23 21:42:41 +0200

    it looks so epic!!!

--- site/ts/login.ts
+++ site/ts/login.ts
@@ -3,16 +3,24 @@ function toggleFormType(): void {
     const form = document.getElementById("credential-form");
     const optionalContent = document.getElementById("optional-form-content");
     const submitButton = document.getElementById("submit-button");
+    const title = document.getElementById("form-title");
+    const linkText = document.getElementById("link-text");
 
     if (form.classList.contains("signin")) {
+        // switch to sign UP form
         optionalContent.classList.remove("hidden");
         form.classList.remove("signin");
         submitButton.innerHTML = "Sign Up";
+        title.innerHTML = "Please sign up";
+        linkText.innerHTML = "sign in";
     }
     else {
+        // switch to sign IN form
         optionalContent.classList.add("hidden");
         form.classList.add("signin");
         submitButton.innerHTML = "Sign In";
+        title.innerHTML = "Please sign in";
+        linkText.innerHTML = "sign up";
     }
 }
 
