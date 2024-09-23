c1190e5cc8dfff44ccc996f57cbe39bc6ce65b73
tobia <tobiaslbeumer@gmail.com>
2024-09-23 11:57:08 +0200

    Javascript en typescript gefixed als het goed is

--- site/ts/99_index.ts
+++ site/ts/99_index.ts
@@ -1,3 +1,8 @@
-function hello() {
-    console.log("hello")
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector('.sidebar');
+    const arrow = document.getElementById('sidebar-toggle');
+
+    arrow.addEventListener('click', function() {
+        sidebar.classList.toggle('sidebar-expanded');
+    });
+});
