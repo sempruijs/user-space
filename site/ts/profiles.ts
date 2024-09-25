7e4aa40982a5d82c46a309f90e539b559490b2e2
Aangepast <tobiaslbeumer@gmail.com>
2024-09-25 14:06:28 +0200

    finished profiles

--- site/ts/profiles.ts
+++ site/ts/profiles.ts
@@ -2,6 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const searchBar = document.getElementById('search-bar');
     const searchIcon = document.getElementById('search-icon');
+    const modal = document.getElementById('profileModal');
+    const closeBtn = document.getElementById('closeModal');
+    const viewProfileButtons = document.querySelectorAll('button[id^="viewProfile"]');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hidden');
+    }
+
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hidden');
+    }
+
+    viewProfileButtons.forEach(button => {
+        button.addEventListener('click', openModal);
+    });
+
+    closeBtn.addEventListener('click', closeModal);
 
     searchBar.addEventListener('focus', function() {
         searchIcon.classList.add('hidden');
