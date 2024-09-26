18ac4a0eff675f965ad3c3faa46a8a40be56da66
Coenicorn <71347039+Coenicorn@users.noreply.github.com>
2024-09-26 09:59:18 +0200

    Login-behaviour (#43)
    * change doctype and script location
    
    * update flake to work with multiple .html files (#26)
    
    * Revert "update flake to work with multiple .html files (#26)" (#27)
    
    This reverts commit b36713e07f8077aa2b61f310b2893b2e6be0856e.
    
    * test (#29)
    
    * Flake-multiple-html (#28)
    
    * change doctype and script location
    
    * update flake to work with multiple .html files (#26)
    
    * Revert "update flake to work with multiple .html files (#26)" (#27)
    
    This reverts commit b36713e07f8077aa2b61f310b2893b2e6be0856e.
    
    * add flake support multiple html files
    
    * test
    
    * it works I thinik

--- site/ts/shared.ts
+++ site/ts/shared.ts
@@ -4,7 +4,6 @@ This file contains code which is *supposed to be (convenient to be)* shared acro
 
 */
 
-
 // https://javascript.info/cookie
 
 // returns the cookie with the given name,
@@ -26,18 +25,37 @@ function setCookie(name: string, value: string, expiresMillis: number) {
     document.cookie = cookie;
 }
 
+interface User {
+    name: string;
+    age: number;
+    email: string
+}
+
 /**
  * Checks if locally stored credentials are valid
  * @returns true on valid, false on invalid login information
  */
-async function checkLocalCredentials(): Promise<boolean> {
+async function checkUsernameExists(username: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
 
-        let username = getCookie("username");
+        // check username with server
+        fetch("http://78.47.165.157:3030/users", {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then(response => {
+    
+                response.forEach((value: User, index: number) => {
+                    if (value.name === username) 
+                    {
+                        resolve(true);
+                        return;
+                    }
+                });
 
-        if (username === undefined) resolve(false);
-        else resolve(true);
+                resolve(false);
 
+            });
     });
 }
 
@@ -45,13 +63,19 @@ async function checkLocalCredentials(): Promise<boolean> {
  * checks if local login information is valid. If yes, return. If no, send user to login page
  */
 async function requestLogin(): Promise<void> {
+    const username = getCookie("username");
 
-    const loginValid = await checkLocalCredentials();
+    if (username === undefined) window.location.replace("login.html");
+
+    const loginValid = await checkUsernameExists(username);
 
     if (loginValid) return;
 
     // login is invalid, send user to login page
-    let newUrl = new URL(window.location.href).host + "/login.html";
-    console.log(newUrl);
-
+    window.location.replace("login.html");
 }
+
+(window as any).custom = {
+    getCookie, setCookie,
+    checkUsernameExists, requestLogin
+};
